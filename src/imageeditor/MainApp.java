ab1675cbaa7fa6bf278e8ecae98b9d38f05ebdde
Berlop24 <berlop24@gmail.com>
2016-10-17 20:00:30 +0200

    Tipos de imagen
    Se han agrupado en una única opción los tipos de imagen que se pueden
    abrir con la aplicación con el fin de hacer más sencilla la búsqueda de
    la imagen

--- src/imageeditor/MainApp.java
+++ src/imageeditor/MainApp.java
@@ -85,9 +85,8 @@ public class MainApp extends Application {
             FileChooser fileChooser = new FileChooser();
 
             //Set extension filter
-            FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
-            FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
-            fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+            FileChooser.ExtensionFilter extFilterIMAGE = new FileChooser.ExtensionFilter("Image files", "*.JPG", "*.JPEG", "*.PNG");
+            fileChooser.getExtensionFilters().addAll(extFilterIMAGE);
 
             //Show open file dialog
             File file = fileChooser.showOpenDialog(null);
